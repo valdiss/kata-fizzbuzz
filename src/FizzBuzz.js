fc7b6117dd0fd330c0555d53616dfc08fa30f79b
valdiss <valentin.dissais@gmail.com>
2016-11-22 09:32:46 +0100

    Il est vivaaaaant!

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,17 +1,16 @@
 function fizzBuzz(i) {
-  var nombre = parseInt(prompt("Entrez un nombre supérieu ou égal à 1:"));
 
-  if (!(nombre%3)){
+  if (!(i%15)){
+    return("FizzBuzz");
+  }
+  else if (!(i%5)){
+    return("Buzz");
+  }
+  else if (!(i%3)){
     return("Fizz");
   }
-  else if (!(nombre%5)){
-    return("Buzz")
-  }
-  else if (!(nombre%15)){
-    return("FizzBuzz")
-  }
   else {
-  	return("J'ai dis un nombre supérieur ou égal à 1...")
+  	return(i)
   }
 }
 
